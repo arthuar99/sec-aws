1b4fcb050b7d71c9449e56fe492ad8cd73d1fae3
arthuar99 <codewizard@gmail.com>
2026-02-08 19:01:21 +0200

    style: Apply terraform fmt to endpoints.tf

--- endpoints.tf
+++ endpoints.tf
@@ -1,10 +1,10 @@
 # SSM VPC Endpoints (Interface)
 resource "aws_vpc_endpoint" "ssm" {
-  vpc_id            = aws_vpc.main.id
-  service_name      = "com.amazonaws.${var.aws_region}.ssm"
-  vpc_endpoint_type = "Interface"
-  subnet_ids        = aws_subnet.private_app[*].id
-  security_group_ids = [aws_security_group.endpoints_sg.id]
+  vpc_id              = aws_vpc.main.id
+  service_name        = "com.amazonaws.${var.aws_region}.ssm"
+  vpc_endpoint_type   = "Interface"
+  subnet_ids          = aws_subnet.private_app[*].id
+  security_group_ids  = [aws_security_group.endpoints_sg.id]
   private_dns_enabled = true
 
   tags = {
@@ -13,11 +13,11 @@ resource "aws_vpc_endpoint" "ssm" {
 }
 
 resource "aws_vpc_endpoint" "ec2messages" {
-  vpc_id            = aws_vpc.main.id
-  service_name      = "com.amazonaws.${var.aws_region}.ec2messages"
-  vpc_endpoint_type = "Interface"
-  subnet_ids        = aws_subnet.private_app[*].id
-  security_group_ids = [aws_security_group.endpoints_sg.id]
+  vpc_id              = aws_vpc.main.id
+  service_name        = "com.amazonaws.${var.aws_region}.ec2messages"
+  vpc_endpoint_type   = "Interface"
+  subnet_ids          = aws_subnet.private_app[*].id
+  security_group_ids  = [aws_security_group.endpoints_sg.id]
   private_dns_enabled = true
 
   tags = {
@@ -26,11 +26,11 @@ resource "aws_vpc_endpoint" "ec2messages" {
 }
 
 resource "aws_vpc_endpoint" "ssmmessages" {
-  vpc_id            = aws_vpc.main.id
-  service_name      = "com.amazonaws.${var.aws_region}.ssmmessages"
-  vpc_endpoint_type = "Interface"
-  subnet_ids        = aws_subnet.private_app[*].id
-  security_group_ids = [aws_security_group.endpoints_sg.id]
+  vpc_id              = aws_vpc.main.id
+  service_name        = "com.amazonaws.${var.aws_region}.ssmmessages"
+  vpc_endpoint_type   = "Interface"
+  subnet_ids          = aws_subnet.private_app[*].id
+  security_group_ids  = [aws_security_group.endpoints_sg.id]
   private_dns_enabled = true
 
   tags = {
